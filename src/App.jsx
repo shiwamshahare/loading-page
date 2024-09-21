634bc82f14da1581aaef8ed42b34aaa9b17b4b37
Shiwam Shahare <com.shahare@gmail.com>
2024-09-22 03:30:26 +0530

    updated commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import LoadingPage from "./LoadingPage";
+
+const App = () => {
+  return (
+    <>
+      <LoadingPage />
+    </>
+  );
+};
+
+export default App;
---
+++ src/LoadingPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Logo from "./Logo";
+
+const LoadingPage = () => {
+  return (
+    <>
+      <div className="bg-black text-white h-screen w-full flex items-center justify-center relative">
+        Welcome to the experience
+      </div>
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{ ease: "easeOut", duration: 2, delay: 5 }}
+        className="loading-page absolute inset-0 bg-[ linear-gradient(to right, #2c5364, #203a43, #0f2027)] w-full h-full flex flex-col gap-6 items-center justify-center bg-white"
+      >
+        <Logo />
+
+        <div className="h-8 overflow-hidden">
+          <motion.div
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 5, ease: [0.34, 1.56, 0.64, 1] }}
+            className="text-xl uppercase ml-5 font-extrabold"
+          >
+            Shiwam Shahare
+          </motion.div>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default LoadingPage;
